383ba4af974acf71d7ef11c09bc41b5b76019b5f
Jagoda <109973414+toster3d@users.noreply.github.com>
2024-01-25 13:07:34 +0100

    Add save function

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
+# Import necessary modules
 from tkinter import *
 from tkinter import filedialog as fd, ttk
-import tkinter.font as tkFont
 import matplotlib.font_manager as fm
 from tkinter.colorchooser import askcolor
 import tkinter.messagebox
 import PIL
 from PIL import Image, ImageDraw, ImageFont, ImageTk
-from PIL import UnidentifiedImageError
-
-from tkinter.colorchooser import askcolor
-from ttkthemes import ThemedTk, ThemedStyle
+from ttkthemes import ThemedTk
 import os
 
+# Constants
 BACKGROUND_COLOR = "#161a1d"
 FILE_PATH = ""
 IMAGE = ""
+IMAGE_COPY = ""
 FONT = "tahoma"
 FONT_PATH = ""
-COLOR = ""
 OPACITY = 1.0
 FONT_SIZE = 70
-ORIGINAL_HEIGHT = 0
-ORIGINAL_WIDTH = 0
+FULL_HEIGHT = 0
+FULL_WIDTH = 0
 HEIGHT = 0
 WIDTH = 0
+ORIGINAL_HEIGHT = 0
+ORIGINAL_WIDTH = 0
 ROTATION = 0
 COLOR = (255, 255, 255)
 
 
+# Function to open a file dialog and update the displayed image
 def open_file_dialog():
     global FILE_PATH
     file_path = fd.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")])
@@ -36,32 +37,54 @@ def open_file_dialog():
         FILE_PATH = file_path
 
 
+# Function to update the displayed image
 def update_image(file_path):
-    global HEIGHT, WIDTH, ORIGINAL_HEIGHT, ORIGINAL_WIDTH, IMAGE
+    global HEIGHT, WIDTH, FULL_HEIGHT, FULL_WIDTH, IMAGE, ORIGINAL_WIDTH, ORIGINAL_HEIGHT
     photo = Image.open(file_path)
+    ORIGINAL_HEIGHT = photo.size[1]
+    ORIGINAL_WIDTH = photo.size[0]
+    print(photo.size[1], photo.size[0])
     photo.thumbnail(size=(800, 600))
     new_image = ImageTk.PhotoImage(photo)
     image_square.config(image=new_image)
     image_square.image = new_image
-    ORIGINAL_HEIGHT = photo.size[1]
-    HEIGHT = ORIGINAL_HEIGHT // 2
-    ORIGINAL_WIDTH = photo.size[0]
-    WIDTH = ORIGINAL_WIDTH // 2
+    FULL_HEIGHT = photo.size[1]
+    HEIGHT = FULL_HEIGHT // 2
+    FULL_WIDTH = photo.size[0]
+    WIDTH = FULL_WIDTH // 2
     IMAGE = photo  # Save the PhotoImage for later use
 
 
-def get_selected_font(*args):
+# Function to get a list of available TTF fonts
+def get_ttf_fonts():
+    """ Get a list of available TTF fonts """
+    ttf_fonts = fm.findSystemFonts(fontpaths=None, fontext='ttf')
+    fonts_list = []
+    formatted_fonts = [x.split("\\")[-1] for x in ttf_fonts]
+    for f in formatted_fonts:
+        if ".otf" not in f:
+            fonts_list.append(f.replace(".ttf", "").replace(".TTF", "").replace(".ttc", ""))
+    return fonts_list
+
+
+# Callback function for font selection
+def get_selected_font():
+    """ Callback function for font selection """
     global FONT
     FONT = font_combobox.get()
     apply_watermark()
 
 
+# Helper function to convert hex color to RGBA
 def hex_to_rgba(hex_color):
+    """ Convert hex color to RGBA """
     hex_color = hex_color.lstrip('#')
     return tuple(int(hex_color[i:i + 2], 16) for i in (0, 2, 4))
 
 
+# Function to choose color
 def choose_color():
+    """ Choose a color """
     global COLOR
     color_dialog = askcolor()
     if color_dialog:
@@ -73,9 +96,12 @@ def choose_color():
 
         color_preview_canvas.config(bg=hex_color)
         COLOR = rgba_color
+    apply_watermark()
 
 
+# Validation function for size input
 def validate_size_input(value, action):
+    """ Validate size input """
     if action == '1':
         try:
             int_value = int(value)
@@ -88,7 +114,9 @@ def validate_size_input(value, action):
     return True
 
 
+# Function to update watermark size
 def update_watermark_size(value):
+    """ Update watermark size.  """
     try:
         float_value = float(value)
         int_value = int(float_value)
@@ -101,55 +129,71 @@ def update_watermark_size(value):
     apply_watermark()
 
 
+# Function to update opacity
 def update_opacity():
+    """ Update opacity. """
     global OPACITY
     OPACITY = float(opacity_input.get())
     apply_watermark()
 
 
+# Function to rotate clockwise
 def clockwise_rotation():
+    """ Rotate clockwise. """
     global ROTATION
     ROTATION -= 20
     apply_watermark()
 
 
+# Function to rotate anticlockwise
 def anticlockwise_rotation():
+    """ Rotate anti-clockwise. """
     global ROTATION
     ROTATION += 20
     apply_watermark()
 
 
+# Function to move text up
 def move_up():
-    global HEIGHT, ORIGINAL_HEIGHT
-    if HEIGHT < ORIGINAL_HEIGHT:
+    """ Move text up. """
+    global HEIGHT, FULL_HEIGHT
+    if HEIGHT < FULL_HEIGHT:
         HEIGHT = max(0, HEIGHT - 10)
     apply_watermark()
 
 
+# Function to move text down
 def move_down():
-    global HEIGHT, ORIGINAL_HEIGHT
-    if HEIGHT < ORIGINAL_HEIGHT:
+    """ Move text down. """
+    global HEIGHT, FULL_HEIGHT
+    if HEIGHT < FULL_HEIGHT:
         HEIGHT = max(0, HEIGHT + 10)
     apply_watermark()
 
 
+# Function to move text left
 def move_left():
-    global WIDTH, ORIGINAL_WIDTH
-    if WIDTH < ORIGINAL_WIDTH:
-        WIDTH = min(ORIGINAL_WIDTH, WIDTH - 10)
+    """ Move text left. """
+    global WIDTH, FULL_WIDTH
+    if WIDTH < FULL_WIDTH:
+        WIDTH = min(FULL_WIDTH, WIDTH - 10)
         apply_watermark()
 
 
+# Function to move text right
 def move_right():
-    global WIDTH, ORIGINAL_WIDTH
-    if WIDTH < ORIGINAL_WIDTH:
-        WIDTH = min(ORIGINAL_WIDTH, WIDTH + 10)
+    """ Move text right. """
+    global WIDTH, FULL_WIDTH
+    if WIDTH < FULL_WIDTH:
+        WIDTH = min(FULL_WIDTH, WIDTH + 10)
         apply_watermark()
 
 
+# Function to apply watermark on the image
 def apply_watermark():
-    global FILE_PATH, IMAGE, FONT_SIZE, HEIGHT, WIDTH, ROTATION, COLOR, OPACITY, FONT, ORIGINAL_HEIGHT, ORIGINAL_WIDTH
-
+    """ Apply watermark on the image. """
+    global FILE_PATH, IMAGE, IMAGE_COPY, FONT_SIZE, HEIGHT, WIDTH, ROTATION, COLOR, OPACITY, FONT, FULL_HEIGHT, \
+        FULL_WIDTH
     try:
         with Image.open(FILE_PATH).convert("RGBA") as original_image:
             original_image.thumbnail(size=(800, 600))
@@ -161,7 +205,7 @@ def apply_watermark():
             opacity_tuple = (int(255 * OPACITY),)
             watermark_fill = COLOR + opacity_tuple
 
-            # Draw text int the watermark field
+            # Draw text in the watermark field
             draw = ImageDraw.Draw(watermark_field)
             font = ImageFont.truetype(FONT, FONT_SIZE)
 
@@ -195,18 +239,42 @@ def apply_watermark():
             image_square.image = photo_image
 
             IMAGE = watermark_output_rgba
+            # Create a copy of the watermarked image with original height and width for saving image
+            IMAGE_COPY = watermark_output_rgba.copy()
+            IMAGE_COPY = IMAGE_COPY.resize((ORIGINAL_WIDTH, ORIGINAL_HEIGHT), Image.LANCZOS)
 
     except FileNotFoundError:
         tkinter.messagebox.showerror("Error", "File not found.")
     except PIL.UnidentifiedImageError:
         tkinter.messagebox.showerror("Error", "Invalid file extension.")
     except Exception as e:
-        print(f"Error: {e}")
+        tkinter.messagebox.showerror("Error", f"{e}.")
         pass
+
+
+def save_image():
+    """ Save the image with watermark. """
+    global IMAGE_COPY
+    try:
+        if IMAGE_COPY:
+            path = fd.asksaveasfilename(confirmoverwrite=True, defaultextension="png",
+                                        filetypes=[("jpeg", ".jpg"), ("png", ".png"), ("bitmap", "bmp"),
+                                                   ("gif", ".gif")])
+
+            if path:
+                if os.path.splitext(path)[1] == ".jpg":
+                    image_to_save = IMAGE_COPY.convert("RGB")
+                    image_to_save.save(path)
+                else:
+                    IMAGE_COPY.save(path)
+                tkinter.messagebox.showinfo("Success", "Image saved successfully!")
+    except Exception as e:
+        tkinter.messagebox.showerror("Error", f"An error occurred while saving the image: {e}")
 
 
 # -------------------------------WINDOW CONFIG---------------------------------------#
 
+# Create themed Tkinter window
 window = ThemedTk(theme="equilux")
 window.title("Image Watermark App")
 window.minsize(height=800, width=1100)
@@ -214,6 +282,7 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # ---------------------------------IMAGE SQUARE----------------------------------#
 
+# Create an empty image for the initial display
 empty_image = Image.new("RGBA", (800, 600), color=BACKGROUND_COLOR)
 image = ImageTk.PhotoImage(empty_image)
 image_square = ttk.Label(window, image=image, borderwidth=3)
@@ -221,17 +290,23 @@ image_square.image = image  # keep a reference
 image_square.grid(column=0, rowspan=15, sticky='w')
 
 # -------------------------------UPLOAD IMAGE BUTTON-------------------------------#
+
+# Style for custom button
 button_style = ttk.Style()
 button_style.configure("Custom.TButton", width=13, background="#fb8500", foreground="#FFAB59",
                        font=("Ubuntu", 13, "bold"))
-upload_button = ttk.Button(window, text="Upload Image", style="Custom.TButton", command=open_file_dialog)
-upload_button.grid(row=15, column=0, pady=10)
+apply_button = ttk.Button(window, text="Upload Image", style="Custom.TButton", command=open_file_dialog)
+apply_button.grid(row=15, column=0, pady=10)
 
 # -------------------------------WATERMARK LABEL------------------------------------#
+
+# Label for watermark configuration
 config_label = ttk.Label(text="Create your Watermark", background=BACKGROUND_COLOR, foreground='#fb8500',
                          font=("Ubuntu", 18, "bold"),
                          justify="center")
 config_label.grid(column=3, row=1, columnspan=4, pady=(0, 15))
+
+# Label and input for watermark text
 watermark_label = ttk.Label(text="Watermark Text:", width=15, padding=[20, 0], background="#161a1d",
                             font=("Ubuntu", 12, "bold"))
 watermark_label.grid(column=3, row=2, sticky='w')
@@ -240,30 +315,28 @@ watermark_input.grid(column=4, row=2, columnspan=3, sticky='w')
 watermark_input.get()
 
 # --------------------------------FONT LABEL------------------------------------------#
-def get_ttf_fonts():
-    # Get a list of available TTF fonts
-    ttf_fonts = fm.findSystemFonts(fontpaths=None, fontext='ttf')
-    fonts_list = []
-    formatted_fonts = [x.split("\\")[-1] for x in ttf_fonts]
-    for f in formatted_fonts:
-        if ".otf" not in f:
-            fonts_list.append(f.replace(".ttf", "").replace(".TTF", "").replace(".ttc", ""))
-    return fonts_list
 
+# Get a list of available fonts and create a sorted list
 available_fonts = get_ttf_fonts()
 sorted_fonts = sorted(available_fonts)
+
+# Label for font selection
 font_label = ttk.Label(text="Font:", width=15, background="#161a1d", font=("Ubuntu", 12, "bold"))
 font_label.grid(column=3, row=3)
-all_fonts = tkFont.families()
+
+# Combobox for font selection
 font_combobox = ttk.Combobox(window, values=sorted_fonts, width=45, font=("Ubuntu", 10))
 font_combobox.grid(column=4, row=3, columnspan=3, sticky='w', pady=(1, 1))
 font_combobox.bind("<<ComboboxSelected>>", get_selected_font)
 font_combobox.set("tahoma")
 
 # --------------------------------COLOR LABEL------------------------------------------#
+
+# Label for color selection
 color_label = ttk.Label(text="Color:", width=15, background=BACKGROUND_COLOR, font=("Ubuntu", 12, "bold"))
 color_label.grid(column=3, row=4)
 
+# Canvas to display color preview
 color_preview_canvas = Canvas(window,
                               width=20,
                               height=20,
@@ -271,15 +344,19 @@ color_preview_canvas = Canvas(window,
                               highlightthickness=0)
 color_preview_canvas.grid(column=6, row=4)
 
+# Entry and button for color input
 color_input = ttk.Entry(width=18, font=("Ubuntu", 10))
 color_input.grid(column=5, row=4, columnspan=2, sticky='w')
-
 choose_color_button = ttk.Button(window, text="Choose Color", command=choose_color)
 choose_color_button.grid(column=4, row=4, sticky='w')
 
 # --------------------------------SIZE LABEL------------------------------------------#
+
+# Label for font size selection
 size_label = ttk.Label(text="Font Size:", background=BACKGROUND_COLOR, width=15, font=("Ubuntu", 12, "bold"))
 size_label.grid(column=3, row=5)
+
+# Scale and entry for font size input
 current_font_size = IntVar(value=70)
 current_font_size.set(FONT_SIZE)
 size_scale = ttk.Scale(window, length=250, from_=3, to=250, orient=HORIZONTAL, command=update_watermark_size,
@@ -292,6 +369,8 @@ size_input = ttk.Entry(width=6, font=("Ubuntu", 10), validate='key', textvariabl
 size_input.grid(column=6, row=5, pady=0)
 
 # --------------------------------OPACITY LABEL------------------------------------------#
+
+# Label for opacity selection
 opacity_label = ttk.Label(text="Opacity:", background=BACKGROUND_COLOR, width=15, font=("Ubuntu", 12, "bold"))
 opacity_label.grid(column=3, row=6)
 
@@ -302,9 +381,12 @@ opacity_input.set(1.0)
 opacity_input.grid(column=4, row=6, sticky='w')
 
 # --------------------------------ROTATION LABEL------------------------------------------#
+
+# Label for rotation selection
 rotation_label = ttk.Label(text="Rotation:", width=15, background=BACKGROUND_COLOR, font=("Ubuntu", 12, "bold"))
 rotation_label.grid(column=3, row=7)
 
+# Buttons for clockwise and anticlockwise rotation
 increment_rotation_button = ttk.Button(text="⭮", command=clockwise_rotation)
 increment_rotation_button.grid(column=4, row=7, sticky='e', padx=20)
 
@@ -312,9 +394,12 @@ decrement_rotation_button = ttk.Button(text="⭯", command=anticlockwise_rotatio
 decrement_rotation_button.grid(column=5, row=7, sticky='w', pady=20)
 
 # --------------------------------POSITION LABEL------------------------------------------#
+
+# Label for position selection
 position_label = ttk.Label(text="Position:", width=15, background=BACKGROUND_COLOR, font=("Ubuntu", 12, "bold"))
 position_label.grid(column=3, row=8)
 
+# Buttons for moving text in different directions
 position_up_button = ttk.Button(text="▲", width=4, command=move_up)
 position_up_button.grid(column=5, row=8)
 
@@ -327,8 +412,8 @@ position_right_button.grid(column=6, row=9, sticky='w')
 position_down_button = ttk.Button(text='▼', width=4, command=move_down)
 position_down_button.grid(column=5, row=10)
 
-upload_button = ttk.Button(window, text="Apply", style="Custom.TButton", command=apply_watermark)
-upload_button.grid(row=14, column=3, columnspan=2)
-upload_button = ttk.Button(window, text="Save Image", style="Custom.TButton")
-upload_button.grid(row=14, column=5, sticky='e')
+apply_button = ttk.Button(window, text="Apply", style="Custom.TButton", command=apply_watermark)
+apply_button.grid(row=14, column=3, columnspan=2)
+save_button = ttk.Button(window, text="Save Image", style="Custom.TButton", command=save_image)
+save_button.grid(row=14, column=5, sticky='e')
 window.mainloop()
