65cf2be403a22f6617a9615705d981a663b0b652
Jagoda <109973414+toster3d@users.noreply.github.com>
2024-01-25 08:51:57 +0100

    Add functionality to apply_watermark function

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ BACKGROUND_COLOR = "#161a1d"
 FILE_PATH = ""
 IMAGE = ""
 FONT = "tahoma"
-print(FONT)
 FONT_PATH = ""
 COLOR = ""
 OPACITY = 1.0
@@ -35,7 +34,6 @@ def open_file_dialog():
     if file_path:
         update_image(file_path)
         FILE_PATH = file_path
-        print(FILE_PATH)
 
 
 def update_image(file_path):
@@ -50,15 +48,12 @@ def update_image(file_path):
     ORIGINAL_WIDTH = photo.size[0]
     WIDTH = ORIGINAL_WIDTH // 2
     IMAGE = photo  # Save the PhotoImage for later use
-    print(HEIGHT)
-    print(WIDTH)
 
 
 def get_selected_font(*args):
     global FONT
     FONT = font_combobox.get()
     apply_watermark()
-    print(FONT)
 
 
 def hex_to_rgba(hex_color):
@@ -78,8 +73,6 @@ def choose_color():
 
         color_preview_canvas.config(bg=hex_color)
         COLOR = rgba_color
-        print(COLOR)
-
 
 
 def validate_size_input(value, action):
@@ -100,132 +93,116 @@ def update_watermark_size(value):
         float_value = float(value)
         int_value = int(float_value)
     except ValueError:
-        int_value = 70  # Domyślna wartość dla przypadku nieudanej konwersji
+        int_value = 70  # Default value
     global FONT_SIZE
     FONT_SIZE = int_value
-    print(FONT_SIZE)
     size_input.delete(0, END)
     size_input.insert(0, str(int_value))
     apply_watermark()
 
 
 def update_opacity():
-    print(opacity_input.get())
     global OPACITY
     OPACITY = float(opacity_input.get())
     apply_watermark()
 
 
-# def update_size(value):
-#     global FONT_SIZE
-#     FONT_SIZE = int(value.get())
-#     apply_watermark()
-#     # size_input.delete(0, END)
-#     # size_input.insert(0, value)
-
-
 def clockwise_rotation():
     global ROTATION
-    ROTATION += 20
-    print(ROTATION)
-    # new_rotation = (current_rotation + 5) % 360  # Zwiększ obrot o 10 stopni
+    ROTATION -= 20
     apply_watermark()
 
 
 def anticlockwise_rotation():
     global ROTATION
-    ROTATION -= 20
-    print(ROTATION)
+    ROTATION += 20
     apply_watermark()
+
 
 def move_up():
     global HEIGHT, ORIGINAL_HEIGHT
-    print(HEIGHT)
     if HEIGHT < ORIGINAL_HEIGHT:
         HEIGHT = max(0, HEIGHT - 10)
-        print(HEIGHT)
     apply_watermark()
-
 
 
 def move_down():
     global HEIGHT, ORIGINAL_HEIGHT
-    print(HEIGHT)
     if HEIGHT < ORIGINAL_HEIGHT:
         HEIGHT = max(0, HEIGHT + 10)
-        print(HEIGHT)
     apply_watermark()
 
 
 def move_left():
     global WIDTH, ORIGINAL_WIDTH
-    print(WIDTH)
     if WIDTH < ORIGINAL_WIDTH:
         WIDTH = min(ORIGINAL_WIDTH, WIDTH - 10)
-        print(WIDTH)
         apply_watermark()
 
 
 def move_right():
     global WIDTH, ORIGINAL_WIDTH
-    print(WIDTH)
     if WIDTH < ORIGINAL_WIDTH:
         WIDTH = min(ORIGINAL_WIDTH, WIDTH + 10)
-        print(WIDTH)
         apply_watermark()
 
+
 def apply_watermark():
-    global FILE_PATH, IMAGE, FONT_SIZE, HEIGHT, WIDTH, ROTATION, COLOR, OPACITY, FONT
+    global FILE_PATH, IMAGE, FONT_SIZE, HEIGHT, WIDTH, ROTATION, COLOR, OPACITY, FONT, ORIGINAL_HEIGHT, ORIGINAL_WIDTH
 
     try:
-        print(f"Otwieranie pliku: {FILE_PATH}")
         with Image.open(FILE_PATH).convert("RGBA") as original_image:
-            print("Tworzenie pola znaku wodnego")
-
-            # Ustaw rozmiar pola znaku wodnego na oryginalny rozmiar obrazu
+            original_image.thumbnail(size=(800, 600))
             watermark_field = Image.new("RGBA", original_image.size, (255, 255, 255, 0))
-
-            print(f"Tekst znaku wodnego: {watermark_input.get()}")
-
-            # Ustaw pozycję tekstu
+            # Set text position
             text_position = (WIDTH, HEIGHT)
 
-            # Zadeklaruj kolor tekstu
+            # Declare text color
             opacity_tuple = (int(255 * OPACITY),)
             watermark_fill = COLOR + opacity_tuple
-            print(f"Kolor tekstu: {watermark_fill}")
 
-            # Narysuj tekst na polu znaku wodnego
+            # Draw text int the watermark field
             draw = ImageDraw.Draw(watermark_field)
             font = ImageFont.truetype(FONT, FONT_SIZE)
+
+            # Get text size
+            text_bbox = draw.textbbox((text_position[0], text_position[1]), f"{watermark_input.get()}", font=font)
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
+
+            text_position = (text_position[0] - text_width // 2, text_position[1] - text_height // 2)
             draw.text(text_position, f"{watermark_input.get()}", fill=watermark_fill, font=font)
 
-            print("Obracanie pola znaku wodnego")
-            watermark_rotation = watermark_field.rotate(ROTATION, expand=True)  # Rozszerz obraz, aby pomieścił obroty
+            watermark_rotation = watermark_field.rotate(ROTATION, expand=False)  # Expand the image to fit rotations
 
-            print("Kompresja alfa")
-            watermark_output = Image.alpha_composite(original_image, watermark_rotation)
+            # Adjust image size to fit in the window
+            new_width = min(original_image.width, watermark_rotation.width)
+            new_height = min(original_image.height, watermark_rotation.height)
 
-            print("Konwersja na RGBA")
+            # Create new watermark and image fields to adjust their sizes
+            resized_watermark = Image.new("RGBA", (new_width, new_height), (255, 255, 255, 0))
+            resized_watermark.paste(watermark_rotation, (0, 0), watermark_rotation)
+            resized_original = Image.new("RGBA", (new_width, new_height), (255, 255, 255, 0))
+            resized_original.paste(original_image, (0, 0), original_image)
+
+            watermark_output = Image.alpha_composite(resized_original, resized_watermark)
+
             watermark_output_rgba = watermark_output.convert("RGBA")
 
-            print("Tworzenie PhotoImage")
             photo_image = ImageTk.PhotoImage(watermark_output_rgba)
 
-            print("Aktualizacja image_square")
             image_square.configure(image=photo_image)
             image_square.image = photo_image
 
             IMAGE = watermark_output_rgba
 
     except FileNotFoundError:
-        tkinter.messagebox.showerror("Błąd", "Brak pliku.")
+        tkinter.messagebox.showerror("Error", "File not found.")
     except PIL.UnidentifiedImageError:
-        tkinter.messagebox.showerror("Błąd", "Nieprawidłowe rozszerzenie pliku.")
+        tkinter.messagebox.showerror("Error", "Invalid file extension.")
     except Exception as e:
-        print(f"Błąd: {e}")
+        print(f"Error: {e}")
         pass
-
 
 
 # -------------------------------WINDOW CONFIG---------------------------------------#
@@ -262,18 +239,16 @@ watermark_input = ttk.Entry(width=47, font=("Ubuntu", 10, "bold"))
 watermark_input.grid(column=4, row=2, columnspan=3, sticky='w')
 watermark_input.get()
 
-
 # --------------------------------FONT LABEL------------------------------------------#
 def get_ttf_fonts():
-    # Pobierz listę dostępnych czcionek TTF
+    # Get a list of available TTF fonts
     ttf_fonts = fm.findSystemFonts(fontpaths=None, fontext='ttf')
     fonts_list = []
-    formated_fonts = [x.split("\\")[-1] for x in ttf_fonts]
-    for f in formated_fonts:
+    formatted_fonts = [x.split("\\")[-1] for x in ttf_fonts]
+    for f in formatted_fonts:
         if ".otf" not in f:
             fonts_list.append(f.replace(".ttf", "").replace(".TTF", "").replace(".ttc", ""))
     return fonts_list
-
 
 available_fonts = get_ttf_fonts()
 sorted_fonts = sorted(available_fonts)
@@ -303,7 +278,6 @@ choose_color_button = ttk.Button(window, text="Choose Color", command=choose_col
 choose_color_button.grid(column=4, row=4, sticky='w')
 
 # --------------------------------SIZE LABEL------------------------------------------#
-
 size_label = ttk.Label(text="Font Size:", background=BACKGROUND_COLOR, width=15, font=("Ubuntu", 12, "bold"))
 size_label.grid(column=3, row=5)
 current_font_size = IntVar(value=70)
@@ -318,22 +292,19 @@ size_input = ttk.Entry(width=6, font=("Ubuntu", 10), validate='key', textvariabl
 size_input.grid(column=6, row=5, pady=0)
 
 # --------------------------------OPACITY LABEL------------------------------------------#
-
 opacity_label = ttk.Label(text="Opacity:", background=BACKGROUND_COLOR, width=15, font=("Ubuntu", 12, "bold"))
 opacity_label.grid(column=3, row=6)
 
-# Spinbox dla wyboru wartości opacity
+# Spinbox to choose opacity values
 opacity_input = ttk.Spinbox(from_=0.0, to=1.0, increment=0.1, format="%.1f", justify="center", width=5,
                             font=("Ubuntu", 10), command=update_opacity)
 opacity_input.set(1.0)
 opacity_input.grid(column=4, row=6, sticky='w')
 
 # --------------------------------ROTATION LABEL------------------------------------------#
-
 rotation_label = ttk.Label(text="Rotation:", width=15, background=BACKGROUND_COLOR, font=("Ubuntu", 12, "bold"))
 rotation_label.grid(column=3, row=7)
 
-# Przyciski do zwiększania i zmniejszania wartości rotation
 increment_rotation_button = ttk.Button(text="⭮", command=clockwise_rotation)
 increment_rotation_button.grid(column=4, row=7, sticky='e', padx=20)
 
@@ -341,7 +312,6 @@ decrement_rotation_button = ttk.Button(text="⭯", command=anticlockwise_rotatio
 decrement_rotation_button.grid(column=5, row=7, sticky='w', pady=20)
 
 # --------------------------------POSITION LABEL------------------------------------------#
-
 position_label = ttk.Label(text="Position:", width=15, background=BACKGROUND_COLOR, font=("Ubuntu", 12, "bold"))
 position_label.grid(column=3, row=8)
 
@@ -362,4 +332,3 @@ upload_button.grid(row=14, column=3, columnspan=2)
 upload_button = ttk.Button(window, text="Save Image", style="Custom.TButton")
 upload_button.grid(row=14, column=5, sticky='e')
 window.mainloop()
-#
